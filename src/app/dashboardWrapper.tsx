5c5d49b8e42dc995d574b80c58c4d348d4b3d5db
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-05-25 18:36:14 +0600

    Navbar

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Navbar from './(components)/Navbar';
+import Sidebar from './(components)/Sidebar';
 
 export default function DashboardWrapper({
   children,
@@ -6,9 +8,12 @@ export default function DashboardWrapper({
   children: React.ReactNode;
 }) {
   return (
-    <div className={`dark flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      Sidebar
-      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-200`}>
+    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+      <Sidebar />
+      <main
+        className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
+      >
+        <Navbar />
         {children}
       </main>
     </div>
